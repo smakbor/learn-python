c5ecda444d32d47f64a5e0d5351d2cb6a3d7e3b0
smakbor <smakbar3965@gmail.com>
2024-03-29 13:52:51 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+# print(thislist[2])
+print(len(thislist))
+for x in thislist:
+    print(x)
+
+
+def printName(name):
+    print("My name is"+" " +name)
+
+
+printName("Akbor")
+
+def sum(a,b):
+    sum =a+b
+    print(sum)
+
+sum(2,3)
+
+mylist = ["apple","banana",'cherry']
+mylist[3] = "Guava"
+print(mylist)
