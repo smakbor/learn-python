6848e4067f8c44b9d697ba1a2f533326db6a4c4f
smakbor <smakbar3965@gmail.com>
2024-04-04 06:29:33 +0600

    practice constructor function inside class

--- oop.py
+++ oop.py
@@ -23,19 +23,26 @@ s1= Student("SM","Akbor",24)
 
 ## freecodecamp class oop
 class Device:
-    def __init__(self,name,price):
-        #ami jkhn e class er instance make korbo, tkhn e __init__ dunder function ta call hobe
+    #ami jkhn e class er instance make korbo, tkhn e __init__ dunder function ta call ho
+    def __init__(self,name:str,price:float,qty:int):
+        # received parameter gulo ke validation korar jonno "Assert" 
+        #negative value is not accepted, this is that validation
+        assert price>=0, f"Error:Price should not be negative"
+        assert qty>=0
+        
+        #These are object attributes 
         self.name = name
         self.price =price
         print("hello i am called automatically")
         print("Price:",self.price,"Name:",self.name)
+        print(f"Name {name} and total price is {price*qty}")
     
     def calculate_total_price(self,x,y):
         #self function hosse nijer je attribute gulo ace oi gula first parameter a pay
         # print(self.price * self.qty) or print(x*y) same
         print(x*y)
 
-laptop = Device("Apple",1000)
+laptop = Device("Apple",-1000,3)
 laptop.name = "Microsoft Surface"
 laptop.price = 50000
 laptop.qty = 5
